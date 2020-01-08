68405262791c81e3fa028e8f0e6c38b25d2b0ecc
Nigrimmist <Nigrimmist@gmail.com>
2020-01-08 23:44:26 +0300

    comments added

--- AwsLamdaRssCore/AppConfig.cs
+++ AwsLamdaRssCore/AppConfig.cs
@@ -6,11 +6,29 @@ namespace AwsLamdaRssCore
 {
     public class AppConfig
     {
+        /// <summary>
+        /// Owner chat id, all news will be sent here to approve
+        /// </summary>
         public static readonly string OwnerChatId = Environment.GetEnvironmentVariable("ChatOwnerId");
+        /// <summary>
+        /// Debug bot token to receive some debug info, errors and etc.
+        /// </summary>
         public static readonly string DebugBotToken = Environment.GetEnvironmentVariable("DebugBotToken");
+        /// <summary>
+        /// Main bot to send messages for approve and to your chats after approve
+        /// </summary>
         public static readonly string MonkeyJobBotToken = Environment.GetEnvironmentVariable("MonkeyJobBotToken");
+        /// <summary>
+        /// Another one chat source to receive news
+        /// </summary>
         public static readonly string PrivateMotoChatId = Environment.GetEnvironmentVariable("PrivateMotoChatId");
+        /// <summary>
+        /// public channel to receive news
+        /// </summary>
         public static readonly string MotoNewsChatId = Environment.GetEnvironmentVariable("MotoNewsChatId");
+        /// <summary>
+        /// public channel's bot to broadcast rss news
+        /// </summary>
         public static readonly string MotoNewsChatBotToken = Environment.GetEnvironmentVariable("MotoNewsChatBotToken");
     }
 }
